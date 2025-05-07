8cd8e762ec155a86059609caa9e88b41e815529c
Alexandru Ion <alexandruion@Alexandrus-MacBook-Pro-2.local>
2025-05-07 12:45:30 +0300

    Update configuration for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const repoName = 'pdd'
-
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? `/${repoName}/` : '',
-  basePath: isProd ? `/${repoName}` : '',
-  output: 'export',
+  output: 'export'
 }
 
 module.exports = withContentlayer(nextConfig) 
